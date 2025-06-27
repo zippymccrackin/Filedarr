e2e587447f9212cae84fc42beabcb841dda6fbe2
Daniel Podobnik <daniel.podobnik@gmail.com>
2025-06-27 07:45:58 -0400

    Modularized the PowerShell script with listeners to hook into. More to come.

---
+++ ps/core/listeners.ps1
@@ -0,0 +1,5 @@
+$Global:ChunkSizeListeners = @()
+$Global:DelayMsListeners = @()
+$Global:ChunkTransferredListeners = @()
+$Global:StagingStartingListeners = @()
+$Global:TransferCompleteListeners = @()
